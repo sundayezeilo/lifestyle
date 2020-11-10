b835cca62b57b00c05217db1aceba97b8435f9ab
ezeilo-su <ezeilo.su@gmail.com>
2020-11-10 19:12:07 +0100

    Create site layout

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,24 +1,58 @@
 require 'rails_helper'
 
-RSpec.feature "StaticPages", type: :feature do
-  describe "GET /about" do
+RSpec.feature 'StaticPages', type: :feature do
+  before { visit '/' }
+
+  describe 'site layout' do
+    it 'should have two links' do
+      expect(page).to have_link(href: '/', count: 2)
+    end
+
+    it 'should have about link' do
+      expect(page).to have_link(href: '/about')
+    end
+
+    it 'should have help link' do
+      expect(page).to have_link(href: '/help')
+    end
+
+    it 'should have contact link' do
+      expect(page).to have_link(href: '/contact')
+    end
+  end
+
+  describe 'GET /about' do
     it 'should get about page with the correct title' do
-      visit "/static_pages/about"
+      click_link 'About'
       expect(page).to have_title('About | Lifestyle Articles Website')
     end
   end
 
-  describe "GET /home" do
+  describe 'GET /' do
     it 'should get home pgae with the correct title' do
-      visit "/static_pages/home"
+      visit '/'
       expect(page).to have_title('Home | Lifestyle Articles Website')
     end
   end
 
-  describe "GET /help" do
+  describe 'GET /' do
+    it 'should get home pgae with the correct title' do
+      click_link 'Home'
+      expect(page).to have_title('Home | Lifestyle Articles Website')
+    end
+  end
+
+  describe 'GET /help' do
     it 'should get help page with the correct title' do
-      visit "/static_pages/help"
+      click_link 'Help'
       expect(page).to have_title('Help | Lifestyle Articles Website')
+    end
+  end
+
+  describe 'GET /contact' do
+    it 'should get contact page with the correct title' do
+      click_link 'Contact'
+      expect(page).to have_title('Contact | Lifestyle Articles Website')
     end
   end
 end
