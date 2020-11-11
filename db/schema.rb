a094690d9cbaea3b50f7f5659525a5696c10fd96
ezeilo-su <ezeilo.su@gmail.com>
2020-11-11 16:15:59 +0100

    Drop-Create DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_132559) do
+ActiveRecord::Schema.define(version: 2020_11_11_084103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "articles", force: :cascade do |t|
-    t.integer "author_id"
-    t.string "title"
-    t.string "text"
-    t.integer "category_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_id"], name: "index_articles_on_author_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.string "priority"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["name"], name: "index_categories_on_name"
-    t.index ["priority"], name: "index_categories_on_priority"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
