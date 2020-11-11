8891c9baefd4137754f3090b007952eed69f7831
ezeilo-su <ezeilo.su@gmail.com>
2020-11-11 13:35:52 +0100

    Create article model

---
+++ app/models/article.rb
@@ -0,0 +1,17 @@
+class Article < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_and_belongs_to_many :category
+  # has_one_attached :image
+  default_scope -> { order(created_at: :desc) }
+  validates :category_id, presence: true
+  validates :author_id, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :text, presence: true, length: { maximum: 300 }
+  # validates :image, content_type: { in: %w[image/jpeg image/gif image/png], message: "must be a valid image format" },
+  # size: { less_than: 5.megabytes,  message: "should be less than 5MB" }
+
+  # Returns a resized image for display.
+  def display_image
+    image.variant(resize_to_limit: [500, 500])
+  end
+end
---
+++ db/migrate/20201111120812_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.integer :author_id
+      t.string :title
+      t.string :text
+      t.integer :category_id
+
+      t.timestamps
+    end
+    add_index :articles, :author_id
+  end
+end
