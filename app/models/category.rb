9adad1d1aef020fb20f0b877bcefb47278232e7b
ezeilo-su <ezeilo.su@gmail.com>
2020-11-11 15:53:56 +0100

    Discard Article and Category models

--- app/models/category.rb
+++
@@ -1,6 +0,0 @@
-class Category < ApplicationRecord
-  has_and_belongs_to_many :articles
-  default_scope -> { order(priority) }
-  validates :priority, presence: true
-  validates :name, presence: true, length: { maximum: 15 }
-end
