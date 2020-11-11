76bf92fbfe362a1bf336ca46a6814b94b2620346
ezeilo-su <ezeilo.su@gmail.com>
2020-11-11 09:20:41 +0100

    Update devise signup and login views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'static_pages#home'
+  
+  root to: 'static_pages#home'
   get  '/help',  to: 'static_pages#help'
   get  '/about',  to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
