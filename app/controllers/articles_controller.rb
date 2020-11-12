79a31bfcce208354932833d3ed6a3c2a06ff1bc3
ezeilo-su <ezeilo.su@gmail.com>
2020-11-12 21:19:25 +0100

    Create articles controller and views

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,10 @@
+class ArticlesController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def create    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :articles, only: %i[new create index]
   root 'static_pages#home'
   devise_for :users
-  get  '/help',  to: 'static_pages#help'
-  get  '/about',  to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 end
