3bdbb16e9cabb7fe71eff6f0551a3dfcf988d5cb
ezeilo-su <ezeilo.su@gmail.com>
2020-11-14 10:29:31 +0100

    Update category model spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,6 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+
+  describe 'validations' do
+    before { @category = Category.create(name: 'fashion', priority: 1) }
+
+    it 'should be valid' do
+      expect(@category).to be_valid
+    end
+
+    it 'should have a name attribute' do
+      expect(@category).to respond_to(:name)
+    end
+
+    it 'should have a priority attribute' do
+      expect(@category).to respond_to(:priority)
+    end
+  end
+
   describe 'articles association' do
     before do
       @user = FactoryBot.build(:user)
@@ -11,6 +28,13 @@ RSpec.describe Category, type: :model do
       Category.create(name: 'business', priority: 2)
       @article.categories << [Category.find_by(priority: 1), Category.find_by(priority: 2)]
       @article.save
+      
+      @category = Category.create(name: 'fashion', priority: 1)
+    end
+
+
+    it 'should have articles association' do
+      expect(@category).to respond_to(:articles)
     end
 
     context 'deleting a category' do
