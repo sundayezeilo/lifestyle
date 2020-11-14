6f5458467b15455c53398a941599d1b82d2db86c
ezeilo-su <ezeilo.su@gmail.com>
2020-11-14 10:16:16 +0100

    Fix rubocop in category model spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   describe 'articles association' do
-
     before do
       @user = FactoryBot.build(:user)
       @user.save
@@ -10,12 +9,11 @@ RSpec.describe Category, type: :model do
       @article = FactoryBot.build(:article, author: @user)
       Category.create(name: 'fashion', priority: 1)
       Category.create(name: 'business', priority: 2)
-      @article.categories << [ Category.find_by(priority: 1), Category.find_by(priority: 2) ]
-      @article.save 
+      @article.categories << [Category.find_by(priority: 1), Category.find_by(priority: 2)]
+      @article.save
     end
 
     context 'deleting a category' do
-
       it 'should destroy the category' do
         Category.find_by(priority: 2).destroy
         expect(Category.find_by(priority: 2)).to be_nil
@@ -33,7 +31,7 @@ RSpec.describe Category, type: :model do
       end
 
       it 'should not destroy any associated articles' do
-        Category.destroy_all 
+        Category.destroy_all
         expect(Article.all).to_not be_empty
       end
     end
