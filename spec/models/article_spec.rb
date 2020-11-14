2d29391b97870d16534ded0071a43b44aea9b66a
ezeilo-su <ezeilo.su@gmail.com>
2020-11-14 10:15:13 +0100

    Finish article model spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,86 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    before do
+      @user = FactoryBot.build(:user)
+      @user.save
+
+      @article = FactoryBot.build(:article, author: @user)
+
+      @category = Category.new(name: 'fashion', priority: 1)
+      @category.save
+    end
+
+    context 'with invalid attributes' do
+      it 'should not be valid without an author' do
+        @article.author = nil
+        @article.save
+        expect(@article).to_not be_valid
+      end
+
+      it 'should not be valid without a title' do
+        @article.title = ' '
+        @article.save
+        expect(@article).to_not be_valid
+      end
+
+      it 'should not be valid without content' do
+        @article.text = ' '
+        @article.save
+        expect(@article).to_not be_valid
+      end
+
+      it 'should not be valid with invalid category' do
+        @article.categories << []
+        @article.save
+        expect(@article).to_not be_valid
+      end
+    end
+
+    context 'with valid attributes' do
+      it 'should be valid' do
+        @article.categories << @category
+        @article.save
+        expect(@article).to be_valid
+      end
+    end
+
+    context 'with valid attributes with more than one category' do
+      before do
+        @second_category = Category.new(name: 'business', priority: 2)
+        @second_category.save
+      end
+
+      it 'should be valid' do
+        @article.categories << @category
+        expect(@article).to be_valid
+      end
+
+      it 'should populate categories' do
+        @article.categories << [@category, @second_category]
+        @article.save
+        expect(@article.reload.categories).to eq([@category, @second_category])
+      end
+    end
+
+    context 'article default scope' do
+      before do
+        @category = Category.new(name: 'business', priority: 2)
+        @category.save
+
+        @older_article = FactoryBot.build(:article, author: @user, created_at: 1.day.ago)
+        @older_article.categories << @category
+        @older_article.save
+
+        @newer_article = FactoryBot.build(:article, author: @user, created_at: 1.hour.ago)
+        @newer_article.categories << @category
+        @newer_article.save
+      end
+
+      it 'should have the right articles in the right order' do
+        expect(Article.all).to eq([@newer_article.reload, @older_article.reload])
+      end
+    end
+  end
 end
