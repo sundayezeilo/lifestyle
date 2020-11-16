b0c7eb946166867907a79ba32891c3759ed1e50c
ezeilo-su <ezeilo.su@gmail.com>
2020-11-17 00:38:05 +0100

    Add eager_load to categories query

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @categories = Category.preload(:articles)
+    @categories = Category.preload(articles: [{ image_attachment: :blob }])
     @highest_voted_article = Article.all.order('votes_count DESC').first
   end
 end
