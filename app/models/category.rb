4f9d4fb2381ba91d6054ff2a78f9c694bfbf0790
ezeilo-su <ezeilo.su@gmail.com>
2020-11-18 21:55:50 +0100

    Change category name attribute max length

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   has_and_belongs_to_many :articles
   default_scope -> { order('priority') }
   validates :priority, presence: true
-  validates :name, presence: true, length: { maximum: 15 }
+  validates :name, presence: true, length: { maximum: 20 }
 
   def name_capitalize
     name.capitalize
