fe36e9de72d3ec663374a16cb04837f3471fc5d3
ezeilo-su <ezeilo.su@gmail.com>
2020-11-21 00:30:15 +0100

    Fix render different image links in different envs

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,7 +1,11 @@
 module CategoriesHelper
   def load_image(article = nil)
     if article&.image&.attached?
-      rails_blob_url(article.image)
+      if Rails.env.production?
+        article.image
+      else
+        rails_blob_url(article.image)
+      end
     else
       'https://www.sortiraparis.com/images/80/83517/577986-visuel-paris-velo-quai-2.jpg'
     end
