b36e589c88ba0ab124060a5264c083f89ca912cf
ezeilo-su <ezeilo.su@gmail.com>
2020-11-20 12:15:48 +0100

    Update name_capitalize class method

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,12 @@ class Category < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
   default_scope -> { order('priority') }
   scope :fetch, -> { preload(articles: [{ image_attachment: :blob }]) }
+
+  def name_capitalize
+    cap_name = []
+    name.split(' ').each do |n|
+      cap_name << n.capitalize
+    end
+    cap_name.join(' ')
+  end
 end
