04acb81bf1887f96c5e140c4b83081152ada5fa3
ezeilo-su <ezeilo.su@gmail.com>
2020-11-21 10:39:37 +0100

    Update article attributes length validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,8 +5,8 @@ class Article < ApplicationRecord
   has_many :votes, dependent: :destroy
   validates :author_id, presence: true
   validates :categories, presence: true
-  validates :title, presence: true, length: { maximum: 50 }
-  validates :text, presence: true, length: { maximum: 500 }
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :text, presence: true, length: { maximum: 1000 }
 
   validates :image, content_type: { in: %w[image/jpeg image/gif image/png], message: 'must be a valid image format' },
                     size: { less_than: 5.megabytes, message: 'should be less than 5MB' }
