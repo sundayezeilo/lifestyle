e00e89db3c9f5cd5e7ba2bd356c9253ca685144d
ezeilo-su <ezeilo.su@gmail.com>
2020-11-21 11:49:31 +0100

    Update article image dev env url

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,7 +1,11 @@
 module CategoriesHelper
   def load_image(article = nil)
     if article&.image&.attached?
-      article.image.service_url
+      if Rails.env.production?
+        article.image.service_url
+      else
+        rails_blob_url(article.image)
+      end
     else
       'https://www.sortiraparis.com/images/80/83517/577986-visuel-paris-velo-quai-2.jpg'
     end
